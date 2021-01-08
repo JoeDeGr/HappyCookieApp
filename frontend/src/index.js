d12c6f2391f84793c2f7c069472cd9ea9939fb96
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-01-08 12:17:03 -0500

    building JS

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+const BASE_URL = "http://localhost:3000"
+const USER_URL = `${BASE_URL}/users`
+const FORTUNE_URL = `${BASE_URL}/fortunes`
+
+document.addEventListener('DOMContentLoaded', (e) =>{
+    fetchFortune();
+});
+
+function fetchFortune(){
+
+}
