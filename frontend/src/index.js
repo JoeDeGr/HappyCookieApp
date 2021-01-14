bec6eed703e3c343131f3058d6601c1c59da8409
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-01-14 15:22:53 -0500

    added a hide function

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 const BASE_URL = "http://localhost:3000"
 const USER_URL = `${BASE_URL}/users`
 const FORTUNE_URL = `${BASE_URL}/fortunes`
+login = document.querySelector("body > div.login")
 
 document.addEventListener('DOMContentLoaded', (e) =>{
     buildPage()
@@ -10,6 +11,15 @@ document.addEventListener('DOMContentLoaded', (e) =>{
 function buildPage(){
     let main = document.querySelector('main')
     let div = document.createElement('div')
+    main.appendChild(div)
 
 
+}
+
+function hide(obj){
+    if (obj.style.display === "none"){
+        obj.style.display = "block";
+    } else {
+        obj.style.display = "none";
+    }
 }
