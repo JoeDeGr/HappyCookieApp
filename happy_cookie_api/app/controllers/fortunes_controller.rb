83ac93590b25463a72e4c190cd4a85f97a44a6d8
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-01-22 16:25:55 -0500

    refactored fortunes#create to eliminate previously delivered fortunes and added a default message if all fortunes have already been delivered.

--- happy_cookie_api/app/controllers/fortunes_controller.rb
+++ happy_cookie_api/app/controllers/fortunes_controller.rb
@@ -6,9 +6,15 @@ class FortunesController < ApplicationController
 
   def create
     user = User.find_by_id(params[:user_id])
-    newFortune = Resource.first
-    binding.pry
-    fortune = Fortune.create(resource_id: newFortune.id, posVibes: newFortune.fortune, subType: newFortune.subType, user_id: user.id)
+    ids = user.resource_ids
+    fortunes = Resource.all
+    newFortunes = fortunes.collect{|fortune| fortune if ids.any?(fortune.id) == false}.compact
+      if newFortunes.length == 0
+        fortune = Fortune.new(id: 0, resource_id: 0, posVibes: "It seems you've gotten to the last cookie we currently have, please check back later!", subType: "default message", user_id: user.id)
+      else 
+        newFortune = newFortunes[rand(newFortunes.length)]
+        fortune = Fortune.create(resource_id: newFortune.id, posVibes: newFortune.fortune, subType: newFortune.subType, user_id: user.id)
+      end
     render json: fortune
   end
 
--- happy_cookie_api/app/models/resource.rb
+++ happy_cookie_api/app/models/resource.rb
@@ -1,10 +1,5 @@
 class Resource < ApplicationRecord
     has_many :fortunes
     has_many :users, through: :fortunes
-
-    def get(user)
-        ids = user.resource_ids
-        fortunes = Resource.all.map(|id| ids.any?(id) == false)
-        return fortunes[rand(fortunes.length)]
-    end    
+  
 end
